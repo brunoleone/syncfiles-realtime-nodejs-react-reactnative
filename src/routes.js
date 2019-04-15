e12e1db692f98c8968e9ad2d82f337a69927788b
Bruno Leone <leonecbruno@gmail.com>
2019-04-14 21:47:51 -0300

    Initilize frontend

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,13 @@
-const express = require('express');
-const multer = require('multer');
-const multerConfig = require('./config/multer');
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const routes = express.Router();
+import Main from './pages/Main';
+import Box from './pages/Box';
 
-const BoxController = require('./controllers/BoxController');
-const FileController = require('./controllers/FileController');
-
-// MÉTODOS:
-// GET/POST/PUT/DELETE
-
-routes.post('/boxes', BoxController.store);
-routes.get('/boxes/:id', BoxController.show);
-
-routes.post('/boxes/:id/files',
-    multer(multerConfig).single('file'),
-    FileController.store);
-
-module.exports = routes;
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Main} />
+        </Switch>
+    </BrowserRouter>
+);
